e6b7bdf3c772aba621a0f8cb5c13aafe3fe9bddf
Juan <juan.isidro.menendez@gmail.com>
2026-06-03 12:29:15 +0200

    main.js import added

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-import {validateEmail, validatePassword, loginUser} from  xxxxxxxx
+//1 auth.js Validación de formulario de login
+import {validateEmail, validatePassword, loginUser} from "./src/scripts/auth.js";
 
 //Mensajes de error
 const ERROR_MESSAGES = {
     INVALID_EMAIL: "Por favor, ingresa un correo electrónico válido.",
     INVALID_PASSWORD: "La contraseña debe tener al menos 8 caracteres e incluir al menos un número.",
 }
+
+
+
+
 
