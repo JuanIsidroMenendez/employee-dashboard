b2c7cd33e6ccec83fb644b9e0b7c5bdbcdedec95
Juan <juan.isidro.menendez@gmail.com>
2026-06-03 11:40:03 +0200

    basic array

---
+++ src/scripts/array.js
@@ -0,0 +1,15 @@
+export const employeesMock = [
+  { id: 1, name: "Sigmun Freud", avatar: "../assets/imgs/Sigmund Freud.jpeg" },
+  { id: 2, name: "Carl Jung", avatar: "../assets/imgs/Carl Jung.jpeg" },
+  { id: 3, name: "Friedrich Nietzsche", avatar: "../assets/imgs/Friedrich Nietzsche.jpeg" },
+  { id: 4, name: "Albert Einstein", avatar: "../assets/imgs/Albert Einstein.jpeg" },
+  { id: 5, name: "Baltasar Gracian", avatar: "../assets/imgs/Baltasar Gracian.jpeg" },
+  { id: 6, name: "Fiodor Dostoevsky", avatar: "../assets/imgs/Fiodor Dostoevsky.jpeg" },
+  { id: 7, name: "Franz Kafka", avatar: "../assets/imgs/Franz Kafka.jpeg" },
+  { id: 8, name: "George Orwell", avatar: "../assets/imgs/George Orwell.jpeg" },
+  { id: 9, name: "Victor Frankl", avatar: "../assets/imgs/Victor Frankl.jpeg" },
+  { id: 10, name: "Willix Pato", avatar: "../assets/imgs/Willix Pato.jpeg" }
+];
+
+// NOTA!!!!!!!!
+//Cuando mergee, las imagenes darán error por cambio de ruta. Deberé cambiar la ruta desde main.js a src/assets/imgs/ para que funcione correctamente.
