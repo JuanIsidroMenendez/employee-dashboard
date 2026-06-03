a060ea7b29503df0c9efa936866c93f7394c7bea
Juan <juan.isidro.menendez@gmail.com>
2026-06-03 13:15:46 +0200

    render.js

---
+++ src/scripts/render.js
@@ -0,0 +1,13 @@
+//Renderizado de empleados en el dashboard
+
+export function renderEmployees(employeesList) {
+    const gallery = document.getElementById('employee-gallery');
+
+    if (!gallery) return; // Si la galería o contenedor no existe, se detiene la función para evitar errores.
+
+    gallery.innerHTML = employeesList.map(employee => `
+        <article class="employee">
+            <img src="${employee.avatar}" alt="${employee.name}"> <h2>${employee.name}</h2>
+        </article>
+    `).join('');
+}
